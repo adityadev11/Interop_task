365174faec807ff17f5d91aa722b48e605e6d80d
darkx-11 <crazyaditya11@gmail.com>
2022-03-13 00:53:35 +0530

    Added aggregateScript.js

---
+++ aggregateScript.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const ethers = require("ethers");
+require("dotenv").config();
+const url = process.env.DB_URL;
+mongoose.connect(url);
+const con = mongoose.connection;
+resultMapping = {};
+
+con.on("open", async () => {
+  console.log("Connected!");
+  var transactionSchema = mongoose.Schema({
+    positionInfo: Array, //Only PositionInfo parameter is being used, hence defined in Schema
+  });
+  var transactionModel = mongoose.model("transactions", transactionSchema);
+  const resultSupply = await transactionModel.aggregate([
+    {
+      $project: { positionInfo: { supply: 1 } },
+    },
+    {
+      $unwind: "$positionInfo.supply",
+    },
+    {
+      $group: {
+        _id: "$positionInfo.supply.symbol",
+        USD: { $sum: { $toDecimal: "$positionInfo.supply.feesInUSD" } },
+        Wei: { $sum: { $toDecimal: "$positionInfo.supply.fees" } },
+      },
+    },
+  ]);
+  for (var i = 0; i < resultSupply.length; i++) {
+    resultMapping[resultSupply[i]._id] = {
+      USD: Number(resultSupply[i].USD),
+      Wei: BigInt(resultSupply[i].Wei),
+    };
+  }
+  const resultWithdraw = await transactionModel.aggregate([
+    {
+      $project: { positionInfo: { withdraw: 1 } },
+    },
+    {
+      $unwind: "$positionInfo.withdraw",
+    },
+    {
+      $group: {
+        _id: "$positionInfo.withdraw.symbol",
+        USD: { $sum: { $toDecimal: "$positionInfo.withdraw.feesInUSD" } },
+        Wei: { $sum: { $toDecimal: "$positionInfo.withdraw.fees" } },
+      },
+    },
+  ]);
+
+  for (var i = 0; i < resultWithdraw.length; i++) {
+    resultMapping[resultWithdraw[i]._id].USD += Number(resultWithdraw[i].USD);
+    resultMapping[resultWithdraw[i]._id].Wei += BigInt(resultWithdraw[i].Wei);
+  }
+  for (key in resultMapping) {
+    resultMapping[key].USD = resultMapping[key].USD.toString();
+    resultMapping[key].Wei = resultMapping[key].Wei.toString();
+  }
+  console.log(resultMapping);
+  con.close();
+});
